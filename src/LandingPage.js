ef1de204f8893a45a41a2a3f6cdf7485e3dd40ba
jamesrbedwell <jamesrbedwell@gmail.com>
2018-11-24 12:55:20 +1100

    landing page

---
+++ src/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './LandingPage.css'
+
+export default class LandingPage extends React.Component {
+
+  constructor() {
+    super()
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <button>IM IN</button>
+        <button>TELL ME MORE</button>
+      </div>
+    )
+  }
+}
