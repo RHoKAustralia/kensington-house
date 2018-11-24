1789fe8542d21c2c5de8e507438f2ac28dfa4f6f
AjayRaveendran <ajay.raveendran@gmail.com>
2018-11-25 10:16:44 +1100

    submitted to the API

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,8 +1,43 @@
 import React from "react";
 import { Questionnaire } from "../components/questionnaire";
 
+function getSubmitUrl(email){
+    return `https://1pf9vw9aw3.execute-api.ap-southeast-2.amazonaws.com/prod/users/${email}/questionnaire`
+}
+
 export class Signup extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {isSubmitting: false, submitError: null, isSuccessful: false}
+    }
+
+    onComplete = async (survey, surveyApi) => {
+        this.state.isSubmitting = true
+        const {data} = survey
+        const url = getSubmitUrl(data.email)
+        try {
+            await fetch(url, {method: 'post', body: JSON.stringify(data)});
+            this.state.isSubmitting = false
+            this.state.isSuccessful = true
+        } catch (e) {
+            this.state.isSubmitting = false
+            this.state.submitError = 'error'
+
+        }
+    }
+
     render() {
-        return <Questionnaire />;
+          
+        if (this.state.isSuccessful) {
+            return <p>Success!</p>    
+        } else {
+            return <>
+            {this.state.isSubmitting && <p>Submitting form. Please wait</p>}
+            {this.state.submitError && <p>Uh-oh! Something went wrong</p>}
+            <Questionnaire onComplete={this.onComplete}/>
+            </>;
+        }
+        
     }
 }
