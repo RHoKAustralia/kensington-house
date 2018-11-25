fd6f6f2b8e61790ffdd1c8c02aaf3ebd97d2459d
jamesrbedwell <jamesrbedwell@gmail.com>
2018-11-25 14:51:11 +1100

    landing page change

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -7,15 +7,24 @@ export class LandingPage extends React.Component {
     render() {
         return (
         <div className="wrapper">
-          <div className="logo-wrapper">
-            <img src={imgurl} alt="" className='logo'/>
-          </div>
-          <div className="button-wrapper">
-            <Link to="/signup" className="landing-page-link">I'M IN</Link>
-            <div className="or">
-              <h1>or</h1>
+          <div className="nav">
+            <div className="logo">
+              <h1>KENSINGTON</h1>
+              <h1>NEIGHBOURHOOD</h1>
+              <h1>HEROES</h1>
             </div>
-            <Link to="/moreinfo" className="landing-page-link">TELL ME MORE</Link>
+            <div className="links">
+              <h3>VOLUNTEER STORIES</h3>
+              <h3 className="link-btn">PARTNER WITH US</h3>
+            </div>
+          </div>
+          <div className="content">
+            <h1>Come volunteer around our House</h1>
+            <h4>A Kensington Neighbourhood House Initiative</h4>
+            <div className="button-wrapper">
+              <Link to="/signup" className="landing-page-link">SIGN ME UP!</Link>
+              <Link to="/moreinfo" className="landing-page-link">TELL ME MORE</Link>
+            </div>
           </div>
         </div>
       )
